79c64e7a8b75bff18626f4b6079554a84c90eddd
idealtechnologyboy <technicideal@gmail.com>
2025-03-26 21:49:26 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*PP Boy team [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*PP Boy team [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp Channels *\n\n*H.S.V and Art Stiker Vibes*`;
             const mg = `🛑 *sri lankan Best botta * 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
